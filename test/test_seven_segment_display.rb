f5338d823df58203cf56b62e3bc2492a7a39b6cf
David Cole <github@davidcole.net>
2010-04-15 16:16:32 -0400

    Add tests for size and error.

--- test/test_seven_segment_display.rb
+++ test/test_seven_segment_display.rb
@@ -19,10 +19,37 @@ class TestSevenSegmentDisplay < Test::Unit::TestCase
   || |  || |  |  |  ||    || |
  —  —     —  —     —  —     — 
     END
+    @all_digits_2 = <<-END
+     ——  ——      ——  ——  ——  ——  ——  —— 
+   |   |   ||  ||   |      ||  ||  ||  |
+   |   |   ||  ||   |      ||  ||  ||  |
+     ——  ——  ——  ——  ——      ——  ——     
+   ||      |   |   ||  |   ||  |   ||  |
+   ||      |   |   ||  |   ||  |   ||  |
+     ——  ——      ——  ——      ——  ——  —— 
+    END
+    @all_digits_5 = <<-END
+        —————  —————         —————  —————  —————  —————  —————  ————— 
+      |      |      ||     ||      |            ||     ||     ||     |
+      |      |      ||     ||      |            ||     ||     ||     |
+      |      |      ||     ||      |            ||     ||     ||     |
+      |      |      ||     ||      |            ||     ||     ||     |
+      |      |      ||     ||      |            ||     ||     ||     |
+        —————  —————  —————  —————  —————         —————  —————        
+      ||            |      |      ||     |      ||     |      ||     |
+      ||            |      |      ||     |      ||     |      ||     |
+      ||            |      |      ||     |      ||     |      ||     |
+      ||            |      |      ||     |      ||     |      ||     |
+      ||            |      |      ||     |      ||     |      ||     |
+        —————  —————         —————  —————         —————  —————  ————— 
+    END
   end
   
-  def test_display    
+  def test_display
     assert_equal( @all_digits, 1234567890.seven_segment )
     assert_equal( @all_digits_backwards, 9876543210.seven_segment )
+    assert_equal( @all_digits_2, 1234567890.seven_segment( 2 ) )
+    assert_equal( @all_digits_5, 1234567890.seven_segment( 5 ) )
+    assert_raise( ArgumentError ) { 1234567890.seven_segment( 0 ) }
   end
 end
