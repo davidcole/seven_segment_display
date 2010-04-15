adcf03dcb4f479192e9e483d96c3a5b54e204f0d
David Cole <github@davidcole.net>
2010-04-15 16:18:48 -0400

    Update demo for size.

--- demo.rb
+++ demo.rb
@@ -1,5 +1,11 @@
 require 'lib/seven_segment_display'
 
-0.upto( 100 ) do | x |
+0.step( 100, 5 ) do | x |
   puts x.seven_segment
+end
+0.step( 100, 5 ) do | x |
+  puts x.seven_segment( 2 )
+end
+0.step( 100, 5 ) do | x |
+  puts x.seven_segment( 5 )
 end
