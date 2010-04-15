7058be3dc722a80b84b1cfbccbad787cbb6b7e12
David Cole <github@davidcole.net>
2010-04-15 13:09:18 -0400

    Convert tests for Integer monkeypatch.

--- test/test_seven_segment_display.rb
+++ test/test_seven_segment_display.rb
@@ -1,14 +1,28 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
-
 class TestSevenSegmentDisplay < Test::Unit::TestCase
 
   include SevenSegmentDisplay
 
   def setup
+    @all_digits = <<-END
+    —  —     —  —  —  —  —  — 
+  |  |  || ||  |    || || || |
+    —  —  —  —  —     —  —    
+  ||    |  |  || |  || |  || |
+    —  —     —  —     —  —  — 
+    END
+    @all_digits_backwards = <<-END
+ —  —  —  —  —     —  —     — 
+| || |  ||  |  | |  |  |  || |
+ —  —     —  —  —  —  —       
+  || |  || |  |  |  ||    || |
+ —  —     —  —     —  —     — 
+    END
   end
   
-  def test_truth
-    puts seven_segment( 1029384756 )
+  def test_display    
+    assert_equal( @all_digits, 1234567890.seven_segment )
+    assert_equal( @all_digits_backwards, 9876543210.seven_segment )
   end
 end
