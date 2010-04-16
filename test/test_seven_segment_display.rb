c9a6570d132d1a5f50f9cdb3d31f3aa32fb56356
David Cole <github@davidcole.net>
2010-04-15 23:14:07 -0400

    Add test for negative numbers.

--- test/test_seven_segment_display.rb
+++ test/test_seven_segment_display.rb
@@ -43,6 +43,22 @@ class TestSevenSegmentDisplay < Test::Unit::TestCase
       ||            |      |      ||     |      ||     |      ||     |
         —————  —————         —————  —————         —————  —————  ————— 
     END
+    @all_digits_negative = <<-END
+       —  — 
+     |  |  |
+ —     —  — 
+     ||    |
+       —  — 
+    END
+    @all_digits_negative_2 = <<-END
+         ——  —— 
+       |   |   |
+       |   |   |
+ ——      ——  —— 
+       ||      |
+       ||      |
+         ——  —— 
+    END
   end
   
   def test_display
@@ -50,6 +66,8 @@ class TestSevenSegmentDisplay < Test::Unit::TestCase
     assert_equal( @all_digits_backwards, 9876543210.seven_segment )
     assert_equal( @all_digits_2, 1234567890.seven_segment( 2 ) )
     assert_equal( @all_digits_5, 1234567890.seven_segment( 5 ) )
+    assert_equal( @all_digits_negative, -123.seven_segment )
+    assert_equal( @all_digits_negative_2, -123.seven_segment( 2 ) )
     assert_raise( ArgumentError ) { 1234567890.seven_segment( 0 ) }
   end
 end
