4dd808508d974414bac56c58b9753bcf8a1e62d8
David Cole <github@davidcole.net>
2010-04-30 10:19:05 -0400

    Update History and version number.

--- lib/seven_segment_display.rb
+++ lib/seven_segment_display.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module SevenSegmentDisplay
-  VERSION = '0.0.4'
+  VERSION = '0.0.5'
 
   TOP = 0
   TOP_BODY = 1
